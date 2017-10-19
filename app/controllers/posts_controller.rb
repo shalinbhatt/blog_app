0211f1b7597c2d15136a5aaa1213e0199a64ef06
Shalin Bhatt <shalinbhatt@Shalins-MacBook-Pro.local>
2017-10-18 21:20:57 -0700

    UI changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 	before_action :find_post, only: [:edit, :update, :show, :delete]
 
 	def index
-		@posts = Post.all
+		@posts = Post.all.order(created_at: :DESC)
 	end
 
 	def new
