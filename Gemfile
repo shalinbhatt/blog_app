5c45f485e210eed159572686bad572f9f34f63ce
Shalin Bhatt <shalinbhatt@Shalins-MacBook-Pro.local>
2017-10-23 12:38:45 -0700

    changes in application.rb and application.js to precompile assets

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+
 gem 'jquery-rails'
 gem "mini_magick"
 gem 'carrierwave'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
 //= require bootstrap-sprockets
 //= require ckeditor/init
-//= require jquery-ujs
-//= require ckeditor-jquery
+//= require_tree .
